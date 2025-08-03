46ae403a5cbe7fc60637e83b964df9eaf6d5a06c
thienbeta <thiennguyenbeta@gmail.com>
2025-08-03 16:09:25 +0700

    fix supportchat
    fix

---
+++ Utils/GoogleApisSettings.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UltraStrore.Utils
+{
+    public class GoogleApisSettings
+    {
+        public string ApiUrl { get; set; }
+        public string ApiKey { get; set; }
+    }
+
+    public class ImageGenerationRequest
+    {
+        [Required]
+        public string TextPrompt { get; set; }
+        [Required]
+        public List<string> ImageBase64 { get; set; }
+    }
+
+    public class ImageGenerationResponse
+    {
+        public string GeneratedImageBase64 { get; set; }
+        public string Message { get; set; }
+    }
+
+    public class TextGenerationRequest
+    {
+        [Required]
+        public string TextPrompt { get; set; }
+    }
+
+    public class TextGenerationResponse
+    {
+        public string GeneratedText { get; set; }
+        public string Message { get; set; }
+    }
+}
